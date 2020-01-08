31798502f07e8aab8077a5bae6e1ff41a3fcac8e
MarkCorazon <markvanderhart1999@gmail.com>
2020-01-08 19:38:42 +0100

    Functions Fields And Comments For Pub & Event Class

--- javaKroeg/src/Event.java
+++ javaKroeg/src/Event.java
@@ -1,3 +1,31 @@
-public class Event {
+import java.util.HashSet;
 
+public class Event {
+    private String name;
+    private HashSet<Visitor> visitors;
+    private HashSet<Artist> artists;
+
+    /**
+     * Gets the name of the event.
+     * @return The event name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Hires an artist and lets the pub pay for the artist.
+     * @param artist What artist is going to preform at the pub
+     */
+    public void hireArtist(Artist artist) {
+
+    }
+
+    public void addVisitor(Visitor visitor) {
+
+    }
+
+    public int getAmountOfVisitors() {
+
+    }
 }
--- javaKroeg/src/Pub.java
+++ javaKroeg/src/Pub.java
@@ -1,2 +1,51 @@
+import Drinks.Drink;
+
+import java.util.ArrayList;
+
 public class Pub {
+    private String name;
+    private double budget;
+    private ArrayList<Event> events;
+    private ArrayList<Drink> drinks;
+
+    /**
+     * Gets the name of the pub.
+     * @return Pub name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Adds an event that will take place at the pub.
+     * @param event Event object
+     */
+    public void addEvent(Event event) {
+
+    }
+
+    /**
+     * Get total budget of the pub.
+     * @return double The total budget of the pub
+     */
+    public double getTotalBudget() {
+
+    }
+
+    /**
+     * Checks if the pub can afford events.
+     * @return if the pub is bankrupt
+     */
+    public boolean checkIfBankrupt() {
+
+    }
+
+    /**
+     * Procures drinks for the events.
+     * @param drink What kind of drink the pub wants to procure
+     * @param amount How much of that drink the pub wants to procure
+     */
+    public void procureDrink(Drink drink, int amount) {
+
+    }
 }
