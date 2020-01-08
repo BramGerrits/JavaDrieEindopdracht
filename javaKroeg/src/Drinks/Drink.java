ad6d90d09f21f8efddff5fad305b27140a156c27
BramGerrits <43609156+BramGerrits@users.noreply.github.com>
2020-01-08 19:29:34 +0100

    Drink klasse

--- javaKroeg/src/Drinks/Drink.java
+++ javaKroeg/src/Drinks/Drink.java
@@ -1,4 +1,24 @@
 package Drinks;
 
-public class Drink {
+abstract class Drink {
+    private int priceInCoins = 0; //Prijs in munten.
+    private double price = 0.0;
+    private boolean isProcured = false;
+    private boolean isOrdered = false;
+
+    public double getPrice(){
+        return price;
+    }
+
+    public double getPriceInCoins(){
+        return priceInCoins;
+    }
+
+//    public void sellTo(Visitor visitor){
+//
+//    }
+//
+//    public void getOrderedBy(Pub pub){
+//
+//    }
 }
