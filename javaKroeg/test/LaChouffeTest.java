39eb25f57f97ee20a020a00511abba33fb6cf7be
BramGerrits <43609156+BramGerrits@users.noreply.github.com>
2020-01-14 10:59:06 +0100

    lachouffe test

--- javaKroeg/test/LaChouffeTest.java
+++ javaKroeg/test/LaChouffeTest.java
@@ -6,14 +6,26 @@ public class LaChouffeTest {
 
     @Test
     public void getPrice() {
+        LaChouffe laChouffe = new LaChouffe();
+        assertEquals(laChouffe.getPrice(), 2.50);
     }
 
     @Test
     public void sellTo() {
+        LaChouffe laChouffe = new LaChouffe();
+        Pub pub = new Pub("kroeg", 1000.00);
+        Visitor visitor = new Visitor();
+
+        pub.procureOneDrink(laChouffe);
+
+        laChouffe.sellTo(pub, visitor);
+        assertEquals(visitor.getDrinks().size(),1);
     }
 
     @Test
     public void getDrinkSound() {
+        LaChouffe laChouffe = new LaChouffe();
+        assertEquals(laChouffe.getDrinkSound(), "KLONK");
     }
 
     @Test
